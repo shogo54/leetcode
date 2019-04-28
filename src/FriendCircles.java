2e45efb4cac75b87f6aed4d9c7c9c6696fc4e9c9
ShogoAkiyama54 <sakiyama@knox.edu>
2019-04-27 23:33:08 -0500

    Added 657 and 547

---
+++ src/FriendCircles.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * @author Shogo Akiyama 
+ * Solved on 04/27/2019
+ * 
+ * 547. Friend Circles
+ * https://leetcode.com/problems/friend-circles/
+ * 
+ * To run the code in LeetCode, take the codes from 
+ * findCircleNum(int[][] M) method and class Node.
+ * 
+ * Runtime: 67 ms, faster than 5.13% of Java online submissions for Friend Circles.
+ * Memory Usage: 45.3 MB, less than 33.56% of Java online submissions for Friend Circles.
+ * 
+ */
+
+public class FriendCircles {
+    public int findCircleNum(int[][] M) {
+    
+        Map<Integer, Node> map = new HashMap<Integer, Node>();
+        Set<Node> toVisit = new HashSet<Node>();
+        for(int i=0; i<M.length; i++){
+            map.put(i, new Node(i));
+            toVisit.add(map.get(i));
+        }
+        
+        for(int i=0; i<M.length; i++){
+            for(int j=i+1; j<M.length; j++){
+                if(M[i][j] == 1){
+                    map.get(i).neighbors.add(map.get(j));
+                    map.get(j).neighbors.add(map.get(i));
+                }
+            }
+        }
+        
+        int count=0;
+        while(!toVisit.isEmpty()){
+            count++;
+            Stack<Node> stack = new Stack<Node>();
+            stack.push(toVisit.iterator().next());
+            while(!stack.isEmpty()){
+                Node curr = stack.pop();
+                toVisit.remove(curr);
+                for(Node n: curr.neighbors){
+                    if(toVisit.contains(n)){
+                        stack.push(n);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    
+    class Node {
+        int index;
+        Set<Node> neighbors;
+        Node(int i){
+            index = i;
+            neighbors = new HashSet<Node>();
+        }
+    }
+    
+}
---
+++ src/RobotReturnToOrigin.java
@@ -0,0 +1,37 @@
+/**
+ * @author Shogo Akiyama 
+ * Solved on 04/20/2019
+ * 
+ * 657. Robot Return to Origin
+ * https://leetcode.com/problems/robot-return-to-origin/
+ * 
+ * To run the code in LeetCode, take the codes from 
+ * judgeCircle(String moves) method.
+ * 
+ * Runtime: 15 ms, faster than 13.65% of Java online submissions for Robot Return to Origin.
+ * Memory Usage: 39.6 MB, less than 19.18% of Java online submissions for Robot Return to Origin.
+ * 
+ */
+
+public class RobotReturnToOrigin {
+	public boolean judgeCircle(String moves) {
+		int[] store = new int[4];
+
+		for (int i = 0; i < moves.length(); i++) {
+			String c = moves.substring(i, i + 1);
+			if (c.equals("U"))
+				store[0]++;
+			else if (c.equals("D"))
+				store[1]++;
+			else if (c.equals("R"))
+				store[2]++;
+			else if (c.equals("L"))
+				store[3]++;
+		}
+
+		if (store[0] == store[1] && store[2] == store[3])
+			return true;
+		else
+			return false;
+	}
+}
