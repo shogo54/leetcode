0ced0d72c7248c12da6215156d8074fa05648322
ShogoAkiyama54 <sakiyama@knox.edu>
2019-06-12 22:22:44 -0500

    Add 1047. Remove All Adjacent Duplicates In String

---
+++ src/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,28 @@
+/**
+ * @author Shogo Akiyama 
+ * Solved on 06/12/2019
+ * 
+ * 1047. Remove All Adjacent Duplicates In String
+ * https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+ * 
+ * To run the code in LeetCode, take the codes from method
+ * String removeDuplicates(String S).
+ * 
+ * Runtime: 166 ms, faster than 17.36% of Java online submissions for Remove All Adjacent Duplicates In String.
+ * Memory Usage: 45.5 MB, less than 100.00% of Java online submissions for Remove All Adjacent Duplicates In String.
+ * 
+ */
+
+public class RemoveAllAdjacentDuplicatesInString {
+
+    public String removeDuplicates(String S) {
+        String result = S;
+        for(int i=0; i<S.length()-1; i++){
+            if(S.charAt(i) == S.charAt(i+1)){
+                result = S.substring(0, i) + S.substring(i+2, S.length());
+                return removeDuplicates(result);
+            }
+        }
+        return result;
+    }
+}
