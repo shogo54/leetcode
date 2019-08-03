03e7bded21ed690318025a87f44b1b923aefd1e4
Shogo Akiyama <sakiyama@knox.edu>
2019-08-03 01:56:11 -0500

    Update ValidAnagram.java

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -18,14 +18,14 @@ import java.util.*;
 public class ValidAnagram {
 
 	public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
-        return (new String(sChar).equals(new String(tChar)));
-    }
+		if (s.length() != t.length()) {
+		    return false;
+		}
+		char[] sChar = s.toCharArray();
+		char[] tChar = t.toCharArray();
+		Arrays.sort(sChar);
+		Arrays.sort(tChar);
+		return (new String(sChar).equals(new String(tChar)));
+	}
 	
 }
