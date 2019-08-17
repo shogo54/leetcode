1733ae9368f5136ffba03b94e4a433165793d2a5
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-16 20:29:35 -0500

    141. Linked List Cycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+/**
+ * @author Shogo Akiyama 
+ * Solved on 08/16/2019
+ * 
+ * 141. Linked List Cycle
+ * https://leetcode.com/problems/linked-list-cycle/
+ * Difficulty: Easy
+ * 
+ * To run the code in LeetCode, take the codes from the following method(s) and properties:
+ * - Set<ListNode> set
+ * - boolean hasCycle(ListNode head).
+ * 
+ * Runtime: 4 ms, faster than 20.24% of Java online submissions for Linked List Cycle.
+ * Memory Usage: 36.8 MB, less than 100.00% of Java online submissions for Linked List Cycle.
+ * 
+ */
+
+public class LinkedListCycle {
+
+    Set<ListNode> set = new HashSet<ListNode>();
+    
+    public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        if(set.contains(head)){
+            return true;
+        }
+        set.add(head);
+        return hasCycle(head.next);
+    }
+	
+    static class ListNode {
+    	int val;
+    	ListNode next;
+    	
+    	ListNode(int x) {
+    		val = x;
+    		next = null;
+    	}
+    }
+    
+}
