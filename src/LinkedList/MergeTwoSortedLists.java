2d4067acc9c01dac55bb2bb0a63ed66ac7edda37
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-21 07:10:34 -0500

    Update MergeTwoSortedLists.java

--- src/LinkedList/MergeTwoSortedLists.java
+++ src/LinkedList/MergeTwoSortedLists.java
@@ -38,41 +38,4 @@ public class MergeTwoSortedLists {
 	    }
 	}
 
-//	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//		if(l1 == null && l2 == null) {
-//			return null;
-//		}
-//		if(l1 == null || l2 == null){
-//            ListNode noneNull = (l1 != null)? l1: l2;
-//            merge(noneNull, noneNull.next, null);
-//            return noneNull;
-//        }
-//			
-//        if(l1.val <= l2.val){
-//            merge(l1, l1.next, l2);
-//            return l1;
-//        }else{
-//            merge(l2, l1, l2.next);
-//            return l2;
-//        }
-//    }
-//    
-//    private ListNode merge(ListNode prev, ListNode n1, ListNode n2){
-//        if(n1 == null && n2 == null){
-//            return null;
-//        }
-//        if(n1 == null || n2 == null){
-//            ListNode noneNull = (n1 != null)? n1: n2;
-//            prev.next = noneNull;
-//            merge(noneNull, noneNull.next, null);
-//            return noneNull;
-//        }
-//        
-//        ListNode smaller = (n1.val <= n2.val)? n1: n2;
-//        ListNode larger = (smaller.equals(n2))? n1: n2;
-//        prev.next = smaller;
-//        merge(smaller, smaller.next, larger);
-//        return smaller;
-//    }
-	
 }
