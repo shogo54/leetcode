f85158f0581397bbe4d1921842cd3790da471675
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-25 23:29:09 -0500

    Add Array Test Cases

--- src/Array/ArrayTest.java
+++ src/Array/ArrayTest.java
@@ -1,5 +1,6 @@
 package Array;
 
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +8,24 @@ class ArrayTest {
 
 	@Test
 	void testJumpGame() throws Exception {
-		
+		assertTrue(new JumpGame().canJump(new int[]{2,3,1,1,4}));
+		assertFalse(new JumpGame().canJump(new int[]{3,2,1,0,4}));
 	}
-
+	
+	@Test
+	void testIntersectionOfTwoArrays() throws Exception {
+		assertArrayEquals(new int[] {2}, new IntersectionOfTwoArrays().intersection(new int[] {1,2,2,1}, new int[] {2,2}));
+		
+		int[] expected = new int[] {9,4};
+		int[] actual = new IntersectionOfTwoArrays().intersection(new int[] {4,9,5}, new int[] {9,4,9,8,4});
+		Arrays.sort(expected);
+		Arrays.sort(actual);
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testTwoSum() throws Exception{
+		assertArrayEquals(new int[] {0,1}, new TwoSum().twoSum(new int[] {2, 7, 11, 15}, 9));
+	}
+	
 }
