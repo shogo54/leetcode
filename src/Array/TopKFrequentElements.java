ef4fae2bf1c7ae90c6efdf3bfcf28b77fb273f05
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-27 00:52:56 -0500

    Update TopKFrequentElements.java

--- src/Array/TopKFrequentElements.java
+++ src/Array/TopKFrequentElements.java
@@ -7,44 +7,44 @@ import java.util.*;
  * Solved on 08/27/2019
  * 
  * 347. Top K Frequent Elements
- * https://leetcode.com/problems/top-k-frequent-elements/
+ * https://leetcode.com/problems/top-k-frequent-elements/ 
  * Difficulty: Medium
  * 
- * To run the code in LeetCode, take the codes from the following method(s) and all properties:
+ * To run the code in LeetCode, take the codes from the following method(s) and all properties: 
  * - List<Integer> topKFrequent(int[] nums, int k).
  * 
- * Runtime: 13 ms, faster than 70.89% of Java online submissions for Top K Frequent Elements.
+ * Runtime: 13 ms, faster than 70.89% of Java online submissions for Top K Frequent Elements. 
  * Memory Usage: 40.8 MB, less than 56.03% of Java online submissions for Top K Frequent Elements.
  * 
  */
 
 public class TopKFrequentElements {
-	
-	public List<Integer> topKFrequent(int[] nums, int k){
+
+	public List<Integer> topKFrequent(int[] nums, int k) {
 		Map<Integer, Integer> count = new HashMap<Integer, Integer>();
-        	Map<Integer, Set<Integer>> reverse = new TreeMap<Integer, Set<Integer>>(Collections.reverseOrder());
-        	List<Integer> list = new LinkedList<Integer>();
-		
-		for(int n: nums) {
-			count.put(n, count.getOrDefault(n, 0)+1);
+		Map<Integer, Set<Integer>> reverse = new TreeMap<Integer, Set<Integer>>(Collections.reverseOrder());
+		List<Integer> list = new LinkedList<Integer>();
+
+		for (int n : nums) {
+			count.put(n, count.getOrDefault(n, 0) + 1);
 		}
-		
-		for(Map.Entry<Integer, Integer> entry: count.entrySet()) {
-            		if(!reverse.containsKey(entry.getValue())){
-                		reverse.put(entry.getValue(), new HashSet<Integer>());
-            		}
-            		reverse.get(entry.getValue()).add(entry.getKey());
+
+		for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+			if (!reverse.containsKey(entry.getValue())) {
+				reverse.put(entry.getValue(), new HashSet<Integer>());
+			}
+			reverse.get(entry.getValue()).add(entry.getKey());
 		}
-        
-        	for(Set<Integer> set: reverse.values()){
-            		list.addAll(set);
-            		k -= set.size();
-            		if(k == 0){
-                		break;
-            		}
-        	}
-		
+
+		for (Set<Integer> set : reverse.values()) {
+			list.addAll(set);
+			k -= set.size();
+			if (k == 0) {
+				break;
+			}
+		}
+
 		return list;
 	}
-	
+
 }
