e15bcd6453fd576eb607bf3439424def9b9188c0
Shogo Akiyama <sakiyama@knox.edu>
2019-08-27 00:50:29 -0500

    Update TopKFrequentElements.java

--- src/Array/TopKFrequentElements.java
+++ src/Array/TopKFrequentElements.java
@@ -22,27 +22,27 @@ public class TopKFrequentElements {
 	
 	public List<Integer> topKFrequent(int[] nums, int k){
 		Map<Integer, Integer> count = new HashMap<Integer, Integer>();
-        Map<Integer, Set<Integer>> reverse = new TreeMap<Integer, Set<Integer>>(Collections.reverseOrder());
-        List<Integer> list = new LinkedList<Integer>();
+        	Map<Integer, Set<Integer>> reverse = new TreeMap<Integer, Set<Integer>>(Collections.reverseOrder());
+        	List<Integer> list = new LinkedList<Integer>();
 		
 		for(int n: nums) {
 			count.put(n, count.getOrDefault(n, 0)+1);
 		}
 		
 		for(Map.Entry<Integer, Integer> entry: count.entrySet()) {
-            if(!reverse.containsKey(entry.getValue())){
-                reverse.put(entry.getValue(), new HashSet<Integer>());
-            }
-            reverse.get(entry.getValue()).add(entry.getKey());
+            		if(!reverse.containsKey(entry.getValue())){
+                		reverse.put(entry.getValue(), new HashSet<Integer>());
+            		}
+            		reverse.get(entry.getValue()).add(entry.getKey());
 		}
         
-        for(Set<Integer> set: reverse.values()){
-            list.addAll(set);
-            k -= set.size();
-            if(k == 0){
-                break;
-            }
-        }
+        	for(Set<Integer> set: reverse.values()){
+            		list.addAll(set);
+            		k -= set.size();
+            		if(k == 0){
+                		break;
+            		}
+        	}
 		
 		return list;
 	}
