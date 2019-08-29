042f9137659f2278d3a48d13f95e6fde8eb09344
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-29 01:00:56 -0500

    Update SubtreeOfAnotherTree.java

--- src/BinarySearchTree/SubtreeOfAnotherTree.java
+++ src/BinarySearchTree/SubtreeOfAnotherTree.java
@@ -21,31 +21,31 @@ import BinarySearchTree.BinarySearchTreeTest.TreeNode;
 public class SubtreeOfAnotherTree {
 
 	public boolean isSubtree(TreeNode s, TreeNode t) {
-        if(t == null && s == null){
-            return true;
-        }else if(s == null){
-            return false;
-        }else if(s != null && t == null){
-            return false;
-        }
-        
-        if(s.val == t.val){
-            boolean b = true;
-            if(s.left != null && t.left != null){
-                b = (s.left.val == t.left.val);
-            }
-            if(s.right != null && t.right != null){
-                b = (s.right.val == t.right.val);
-            }
-            if(b == true){
-                b = isSubtree(s.left, t.left) && isSubtree(s.right, t.right);
-            }
-            if(b == true){
-                return true;
-            }
-        }
-        
-        return isSubtree(s.left, t) || isSubtree(s.right, t);
-    }
-	
+		if (t == null && s == null) {
+			return true;
+		} else if (s == null) {
+			return false;
+		} else if (s != null && t == null) {
+			return false;
+		}
+
+		if (s.val == t.val) {
+			boolean b = true;
+			if (s.left != null && t.left != null) {
+				b = (s.left.val == t.left.val);
+			}
+			if (s.right != null && t.right != null) {
+				b = (s.right.val == t.right.val);
+			}
+			if (b == true) {
+				b = isSubtree(s.left, t.left) && isSubtree(s.right, t.right);
+			}
+			if (b == true) {
+				return true;
+			}
+		}
+
+		return isSubtree(s.left, t) || isSubtree(s.right, t);
+	}
+
 }
