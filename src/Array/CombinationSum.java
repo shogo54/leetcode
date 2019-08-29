13c510863872f4509f84345541694e15e46ffc24
ShogoAkiyama54 <sakiyama@knox.edu>
2019-08-29 14:24:20 -0500

    Update CombinationSum.java

--- src/Array/CombinationSum.java
+++ src/Array/CombinationSum.java
@@ -14,8 +14,8 @@ import java.util.*;
  * - List<List<Integer>> combinationSum(int[] candidates, int target)
  * - void combine(int[] cands, int t, List<Integer> sofar, int last).
  * 
- * Runtime: 7 ms, faster than 19.70% of Java online submissions for Combination Sum.
- * Memory Usage: 38.8 MB, less than 38.52% of Java online submissions for Combination Sum.
+ * Runtime: 4 ms, faster than 66.51% of Java online submissions for Combination Sum.
+ * Memory Usage: 39.1 MB, less than 28.15% of Java online submissions for Combination Sum.
  * 
  */
 
@@ -24,18 +24,19 @@ public class CombinationSum {
 	private Set<List<Integer>> result = new HashSet<List<Integer>>();
 
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		combine(candidates, target, new ArrayList<Integer>(), -1);
+		combine(candidates, target, new ArrayList<Integer>(), 0);
 		return new ArrayList<List<Integer>>(result);
 	}
 
 	private void combine(int[] cands, int t, List<Integer> sofar, int last) {
 		if (t == 0) {
 			result.add(new ArrayList<Integer>(sofar));
+			return;
 		}
-		for (Integer i : cands) {
-			if (i >= last && t - i >= 0) {
-				sofar.add(i);
-				combine(cands, t - i, sofar, i);
+		for (int i=last; i<cands.length; i++) {
+			if (t - cands[i] >= 0) {
+				sofar.add(cands[i]);
+				combine(cands, t - cands[i], sofar, i);
 				sofar.remove(sofar.size() - 1);
 			}
 		}
