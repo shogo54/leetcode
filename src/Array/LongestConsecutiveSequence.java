2274e7c55fe8a554a7675ba1f35ea9c96d91a4b3
ShogoAkiyama54 <sakiyama@knox.edu>
2019-09-24 11:20:11 -0500

    Add 128. Longest Consecutive Sequence

---
+++ src/Array/LongestConsecutiveSequence.java
@@ -0,0 +1,82 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @author Shogo Akiyama 
+ * Solved on 09/23/2019
+ * 
+ * 128. Longest Consecutive Sequence
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * Difficulty: Hard
+ * 
+ * To run the code in LeetCode, take the codes from the following method(s) and all properties:
+ * - int longestConsecutive(int[] nums)
+ * - class Group.
+ * 
+ * Runtime: 72 ms, faster than 8.41% of Java online submissions for Longest Consecutive Sequence.
+ * Memory Usage: 38.8 MB, less than 44.83% of Java online submissions for Longest Consecutive Sequence.
+ * 
+ */
+
+public class LongestConsecutiveSequence {
+	
+	public int longestConsecutive(int[] nums) {
+		HashMap<Integer, Group> nodes = new HashMap<Integer, Group>();
+		for (int i : nums) {
+			System.out.println(i);
+			if (nodes.containsKey(i)) {
+				continue;
+			}
+			if (nodes.containsKey(i - 1)) {
+				if (nodes.containsKey(i + 1)) {
+					nodes.put(i, nodes.get(i - 1).add().merge(nodes.get(i + 1)));
+				} else {
+					nodes.put(i, nodes.get(i - 1).add());
+				}
+
+			} else if (nodes.containsKey(i + 1)) {
+				nodes.put(i, nodes.get(i + 1).add());
+
+			} else {
+				nodes.put(i, new Group(1));
+			}
+		}
+
+		int max = 0;
+		for (Group group : nodes.values()) {
+			if (group.num > max) {
+				max = group.num;
+			}
+		}
+
+		return max;
+	}
+
+	static class Group {
+		int num;
+		boolean active;
+		Group parent;
+
+		Group(int n) {
+			num = n;
+			active = true;
+		}
+
+		Group merge(Group g) {
+			num += g.num;
+			g.active = false;
+			g.parent = this;
+			return this;
+		}
+
+		Group add() {
+			if (active) {
+				num++;
+				return this;
+			} else {
+				return parent.add();
+			}
+		}
+	}
+}
