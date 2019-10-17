f3e3b9046a5c9b6b8b34ef91ecdc789081fe24d1
ShogoAkiyama54 <sakiyama@knox.edu>
2019-10-17 02:30:20 -0500

    Add test cases for 322. Coin Change

--- src/array/ArrayTest.java
+++ src/array/ArrayTest.java
@@ -115,6 +115,17 @@ class ArrayTest{
 		assertEquals(11.5, finder2.findMedian());
 	}
 
+	/**
+	 * Test cases for 322. Coin Change
+	 * @see CoinChange
+	 */
+	@Test
+	void testCoinChange() throws Exception{
+		assertEquals(3, new CoinChange().coinChange(new int[] {1,2,5}, 11));
+		assertEquals(-1, new CoinChange().coinChange(new int[] {2}, 3));
+		assertEquals(-1, new CoinChange().coinChange(new int[0], 11));
+	}
+
 	@Test
 	void testJumpGame() throws Exception {
 		assertTrue(new JumpGame().canJump(new int[]{2,3,1,1,4}));
---
+++ src/array/CoinChange.java
@@ -0,0 +1,38 @@
+package array;
+
+/**
+ * @author Shogo Akiyama 
+ * Solved on 08/15/2019
+ * 
+ * 322. Coin Change
+ * https://leetcode.com/problems/coin-change/
+ * difficulty: Medium
+ * 
+ * Apporach: Dynamic Programming (Bottom up)
+ * Runtime: 10 ms, faster than 71.40% of Java online submissions for Coin Change.
+ * Memory Usage: 36 MB, less than 94.08% of Java online submissions for Coin Change.
+ * 
+ * @see ArrayTest#testCoinChange()
+ */
+public class CoinChange {
+
+	public int coinChange(int[] coins, int amount) {
+		int[] mins = new int[amount + 1];
+		for (int i = 1; i < amount + 1; i++) {
+			int min = Integer.MAX_VALUE;
+			for (int j = 0; j < coins.length; j++) {
+				int index = i - coins[j];
+				if (index >= 0 && mins[index] != -1 && mins[index] < min) {
+					min = mins[index];
+				}
+			}
+			if (min != Integer.MAX_VALUE) {
+				mins[i] = min + 1;
+			} else {
+				mins[i] = -1;
+			}
+		}
+		return mins[amount];
+	}
+
+}
