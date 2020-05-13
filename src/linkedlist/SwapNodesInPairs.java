839f3454b3d362143556024f17177c69d3d7fa82
Shogo Akiyama <shogo.a.0504@gmail.com>
2020-05-12 20:22:08 -0500

    Update SwapNodesInPairs.java

--- src/linkedlist/SwapNodesInPairs.java
+++ src/linkedlist/SwapNodesInPairs.java
@@ -1,4 +1,22 @@
-public class Solution {
+package linkedlist;
+
+import linkedlist.LinkedListTest.ListNode;
+
+/**
+ * @author Shogo Akiyama
+ * Solved on 05/12/2020 
+ * 
+ * 24. Swap Nodes in Pairs
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
+ * Difficulty: Medium
+ * 
+ * Approach: Recursion
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Swap Nodes in Pairs.
+ * Memory Usage: 37 MB, less than 5.50% of Java online submissions for Swap Nodes in Pairs.
+ * 
+ * @see LinkedListTest#testSwapNodesInPairs()
+ */
+public class SwapNodesInPairs {
     
     public ListNode swapPairs(ListNode head) {
         if(head == null){
