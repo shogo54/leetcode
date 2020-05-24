83eeaf879965ab403b6c29eb6b200e5e46fb152b
shogo54 <sakiyama@knox.edu>
2020-05-24 04:02:52 -0500

    Update FirstUniqueCharacterInAString.java

--- src/string/FirstUniqueCharacterInAString.java
+++ src/string/FirstUniqueCharacterInAString.java
@@ -2,7 +2,8 @@ package string;
 
 /**
  * @author Shogo Akiyama 
- * Solved on 10/21/2019
+ * Solved on 10/21/2019 and 05/24/2020
+ * Time: 11m30s
  * 
  * 387. First Unique Character in a String
  * https://leetcode.com/problems/first-unique-character-in-a-string/
@@ -11,6 +12,10 @@ package string;
  * Approach: Iteration & Hash Table
  * Runtime: 7 ms, faster than 89.19% of Java online submissions for First Unique Character in a String.
  * Memory Usage: 38.1 MB, less than 98.57% of Java online submissions for First Unique Character in a String.
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * Where the n is the length of the string
  * 
  * @see StringTest#testFirstUniqueCharacterInAString()
  */
@@ -21,15 +26,15 @@ public class FirstUniqueCharacterInAString {
 			return 0;
 		}
 
-		char[] chars = s.toCharArray();
 		int[] firsts = new int[26];
 		int min = Integer.MAX_VALUE;
 
-		for (int i = 0; i < chars.length; i++) {
-			if (firsts[chars[i] - 'a'] == 0) {
-				firsts[chars[i] - 'a'] = i + 1;
+		for (int i = 0; i < s.length(); i++) {
+			int v = s.charAt(i) - 'a';
+			if (firsts[v] == 0) {
+				firsts[v] = i + 1;
 			} else {
-				firsts[chars[i] - 'a'] = -1;
+				firsts[v] = -1;
 			}
 		}
 
