72f6a252905c8e61e1230c039a9cb54c3216fb79
shogo54 <sakiyama@knox.edu>
2020-06-19 20:47:02 -0500

    Update FizzBuzz.java

--- src/integer/FizzBuzz.java
+++ src/integer/FizzBuzz.java
@@ -19,22 +19,21 @@ public class FizzBuzz {
 
 	public List<String> fizzBuzz(int n) {
 		List<String> list = new ArrayList<String>();
-		
-		for (int i = 1; i < n + 1; i++) {
-			if (i % 3 != 0 && i % 5 != 0) {
-				list.add(String.valueOf(i));
-			} else {
-				String s = "";
-				if (i % 3 == 0) {
-					s += "Fizz";
-				}
-				if (i % 5 == 0) {
-					s += "Buzz";
-				}
-				list.add(s);
+
+		for (int i = 1; i <= n; i++) {
+			String s = "";
+			if (i % 3 == 0) {
+				s += "Fizz";
 			}
+			if (i % 5 == 0) {
+				s += "Buzz";
+			}
+			if (s.equals("")) {
+				s = String.valueOf(i);
+			}
+			list.add(s);
 		}
-		
+
 		return list;
 	}
 
